5233a4ecd87abfc4f87062fffbc6bc49b3f5884d
Fadigh672 <151573513+Fadigh672@users.noreply.github.com>
2024-01-16 21:52:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var loader = new GLTFLoader();
+
+loader.load('/beautyProducts.gltf', function (gltf) {
+    // Adjust the position, rotation, or scale of the loaded model if needed
+    gltf.scene.position.set(0, 0, 0);
+    gltf.scene.rotation.set(0, 0, 0);
+    gltf.scene.scale.set(1, 1, 1);
+
+    scene.add(gltf.scene);
+});
+
+
+  // Set up camera position
+  camera.position.set(9,12,3);
+  camera.lookAt(0,0,0);
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+  scene.add(ambientLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
+
+window.addEventListener('resize', function () {
+  var newWidth = window.innerWidth;
+  var newHeight = window.innerHeight;
+
+  camera.aspect = newWidth / newHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(newWidth, newHeight);
+});
